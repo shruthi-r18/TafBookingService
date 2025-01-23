7d268c7eb04a89201886e7b1a6c15083d22b2275
Shruthi R <shruthir@tekarch.com>
2025-01-23 00:11:52 -0800

    working code

--- src/main/java/com/backendExam/TafBookingService/TafBookingServiceApplication.java
+++ src/main/java/com/backendExam/TafBookingService/TafBookingServiceApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TafBookingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TafBookingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TafBookingServiceApplication.class, args);
+    }
 
 }
